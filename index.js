baa952130b99ba14cc033a13540ede1c7979c524
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-12 15:49:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,11 @@ function produceTipCalculator(percentage) {
 
 
 function createDriver() {
-  
+  let driverId = 0;
+  return class {
+    constructor(name) {
+      this.id = ++driverId;
+      this.name = name;
+    }
+  }
 }
